4ce235ef65eb2ebd94f29d5a74fcebb803831329
fahrialfan <136087500+fahrialfan@users.noreply.github.com>
2023-06-09 20:46:39 +0700

    Add files via upload

---
+++ latihanSatu/Object Repository/chooseItems/itemTwo.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>itemTwo</name>
+   <tag></tag>
+   <elementGuidId>543d700f-8df0-42c4-b676-8485be392dba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'add-to-cart-sauce-labs-bolt-t-shirt']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>add-to-cart-sauce-labs-bolt-t-shirt</value>
+      <webElementGuid>75f82ebc-7969-4f1f-b36c-40e5dc3d0d14</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ latihanSatu/Object Repository/formLogin/buttonLogin.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>buttonLogin</name>
+   <tag></tag>
+   <elementGuidId>c74cbe9e-fb47-4de1-a779-fd3e2be5579b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'login-button']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>login-button</value>
+      <webElementGuid>640bc252-1612-467c-a3f1-ed1b7ac92a85</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
